c54602208d5572b4b8c69edaf239ac9d2d964346
Anastasios Loumpas <a.loumpas@gmail.com>
2017-01-22 22:40:49 +0200

    Add javascript that append random elements

--- app/helpers/devices_helper.rb
+++ app/helpers/devices_helper.rb
@@ -2,7 +2,7 @@ module DevicesHelper
   def create_properties_prefix(model)
     # new_or_existing = model.new_record? ? 'new' : 'existing'
 
-    new_or_existing = :new
+    new_or_existing = :fix_me
 
     prefix = "device[#{new_or_existing}_property]"
   end
