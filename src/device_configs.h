fc98be3596632548f8787a3a0b0cf8f67296266d
Anastasios Loumpas <a.loumpas@gmail.com>
2017-05-21 13:48:17 +0300

    Make device to send init info about device on setup

--- src/device_configs.h
+++ src/device_configs.h
@@ -1,5 +1,5 @@
-const char DEVICE_SERIAL_NUMBER[] = { "65466434534534534" };
+const char DEVICE_SERIAL_NUMBER[] = { "555769752436185847" };
+const char DEVICE_FIRMWARE_VERSION[] = { "0.0.1" };
 const char DEVICE_FRIENDLY_NAME[] = { "home-auto-dev534" };
 const char MQTT_USERNAME[] = { "asdf" };
 const char MQTT_PASSWORD[] = { "foobar" };
-const char DEVICE_FIRMWARE_VERSION[] = { "0.0.1" };
--- src/home-automation-arduino-client.cpp
+++ src/home-automation-arduino-client.cpp
@@ -10,6 +10,13 @@
 byte mac[] = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
 IPAddress deviceIp(10, 168, 10, 100); // client address
 IPAddress mqttServerIp(10, 168, 10, 50);
+char applicationServerUrl[] = "home-auto.eu";
+int applicationServerPort = 1024;
+byte sendPostDataBuffer[200] = {0};
+String postDataToSend;
+String postQuery;
+char postBuffer[250];
+int result;
 
 EthernetClient ethClient;
 PubSubClient mqttClient(ethClient);
@@ -31,10 +38,9 @@ void mqttReceiveMsgCallback(char* topic, byte* payload, unsigned int length) {
   } else if ((int)payload[0] == 107) {
     digitalWrite(relayPin, LOW);
   }
-
 }
 
-void mqttConnectionCallback() {
+void mqttConnectToBrokerCallback() {
 
   uint32_t lastTryMqttConnection = millis();
   int reqRes;
@@ -70,16 +76,29 @@ void mqttConnectionCallback() {
 void setup() {
   pinMode(relayPin, OUTPUT);
 
+  Serial.begin(115200);
+
   mqttClient.setServer(mqttServerIp, 1883);
   mqttClient.setCallback(mqttReceiveMsgCallback);
 
-  Serial.begin(115200);
-
+  Serial.print(F("Device Name: "));
+  Serial.println(DEVICE_FRIENDLY_NAME);
   Serial.print(F("Device S/N: "));
   Serial.println(DEVICE_SERIAL_NUMBER);
-
   Serial.print(F("Device Firmware: "));
   Serial.println(DEVICE_FIRMWARE_VERSION);
+
+  postDataToSend += "device_uid=";
+  postDataToSend += DEVICE_SERIAL_NUMBER;
+  postDataToSend += "&";
+  postDataToSend += "1=";
+  postDataToSend += DEVICE_SERIAL_NUMBER;
+  postDataToSend += "&";
+  postDataToSend += "2=";
+  postDataToSend += DEVICE_FIRMWARE_VERSION;
+  postDataToSend += "&";
+  postDataToSend += "3=";
+  postDataToSend += DEVICE_FRIENDLY_NAME;
 
   Serial.println(F("Trying to get IP from DHCP..."));
   if (Ethernet.begin(mac) == 0) {
@@ -88,33 +107,80 @@ void setup() {
       delay(10000);
     }
   }
-  //  Ethernet.begin(mac, deviceIp);
   Serial.print("IP: ");
   Serial.println(Ethernet.localIP());
 
+  digitalWrite(relayPin, HIGH);
+
+  Serial.println("Trying to connect to application server...");
+  result = ethClient.connect(applicationServerUrl, applicationServerPort);
+  if ( result != 1) {
+    Serial.print("Connection failed with msg: ");
+    Serial.println(result);
+    while (1) {
+      Serial.println("Please reboot your device");
+      delay(10000);
+    }
+  }
+  if (ethClient.connected() ) {
+    Serial.println("Connected to application server");
+  }
+
+
+  postQuery += "POST /devices/statusUpdate HTTP/1.1\r\n";
+  postQuery += "HOST: ";
+  postQuery += applicationServerUrl;
+  if (applicationServerPort != 80) {
+    postQuery += ":";
+    postQuery += applicationServerPort;
+  }
+  postQuery += "\r\n";
+  postQuery += "Content-length: ";
+  postQuery += postDataToSend.length();
+  postQuery += "\r\n";
+  postQuery += "Content-type: application/x-www-form-urlencoded\r\n\r\n";
+  postQuery += postDataToSend;
+
+  Serial.println(postQuery);
+
+  postQuery.toCharArray(postBuffer, postQuery.length()+1);
+
+  Serial.println(postQuery.length());
+
+  Serial.write(postBuffer, postQuery.length());
+  Serial.println();
+
+  ethClient.write(postBuffer, postQuery.length());
+
 }
 
-uint32_t beginWaitMqttConnection;
-int mqttConnected = 1; // 0 not connected, 1 conneected // default 1 in order to display the message and init the variable
+// uint32_t beginWaitMqttConnection;
+// int mqttConnected = 1; // 0 not connected, 1 conneected // default 1 in order to display the message and init the variable
 
 void loop() {
 
-  if (!mqttClient.connected()) {
-    if (mqttConnected == 1) {
-      mqttConnected = 0;
-      beginWaitMqttConnection = millis();
-      Serial.print(F("Inside main loop, mqtt disconected: "));
-      Serial.println(beginWaitMqttConnection);
-    }
-    mqttConnectionCallback();
-  } else {
-    if (mqttConnected == 0) {
-      mqttConnected = 1;
-      Serial.print(F("Time passed in order to get MQTT connection: "));
-      Serial.print(millis() - beginWaitMqttConnection);
-      Serial.println(" ms");
-    }
-    mqttClient.loop();
+
+  while(1) {
+    Serial.println("Please Reboot...");
+    delay(10000);
   }
+
+  // if (!mqttClient.connected()) {
+  //   if (mqttConnected == 1) {
+  //     mqttConnected = 0;
+  //     beginWaitMqttConnection = millis();
+  //     Serial.print(F("Inside main loop, mqtt disconected: "));
+  //     Serial.println(beginWaitMqttConnection);
+  //   }
+  //   mqttConnectToBrokerCallback();
+  // } else {
+  //   if (mqttConnected == 0) {
+  //     mqttConnected = 1;
+  //     Serial.print(F("Time passed in order to get MQTT connection: "));
+  //     Serial.print(millis() - beginWaitMqttConnection);
+  //     Serial.println(" ms");
+  //   }
+  //   mqttClient.loop();
+  // }
 
 }
