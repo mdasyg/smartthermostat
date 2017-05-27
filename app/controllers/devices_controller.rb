5559f0c331b033cc91de49d6d2543d829199380d
Anastasios Loumpas <a.loumpas@gmail.com>
2017-05-27 23:10:12 +0300

    Refactor the create and update process on devices controller

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -30,8 +30,6 @@ class DevicesController < ApplicationController
   # POST /devices
   # POST /devices.json
   def create
-    @value_types    = ValueType.all
-
     # clear out the params
     secure_params   = device_params
 
@@ -39,8 +37,8 @@ class DevicesController < ApplicationController
     @device         = Device.new(name: secure_params[:name], location: secure_params[:location], description: secure_params[:description])
     @device.user_id = current_user.id
 
-    if secure_params.has_key?(:new_properties)
-      secure_params[:new_properties].each do |key, new_prop|
+    if secure_params.has_key?(:properties)
+      secure_params[:properties].each do |key, new_prop|
         @device.device_properties.build(new_prop)
       end
     end
@@ -51,6 +49,7 @@ class DevicesController < ApplicationController
         format.html {redirect_to @device, notice: 'Device was successfully created.'}
         format.json {render :show, status: :created, location: @device}
       else
+        @value_types = ValueType.all
         format.html {render :new}
         format.json {render json: @device.errors, status: :unprocessable_entity}
       end
@@ -61,54 +60,77 @@ class DevicesController < ApplicationController
   # PATCH/PUT /devices/1
   # PATCH/PUT /devices/1.json
   def update
-
     # CHECKLATER: check if we need some validations on existing ids.
 
     # clear out the params
-    secure_params                = device_params
+    secure_params              = device_params
 
     # get the stored properties
-    stored_device_property_ids   = @device.device_properties.ids
+    stored_device_property_ids = @device.device_properties.ids
 
-    # found the existings from post
-    existing_device_properties   = secure_params.has_key?(:existing_properties) ? secure_params[:existing_properties] : nil
-    existing_device_property_ids = []
-    if existing_device_properties
-      existing_device_properties
-          .keys
-          .each {|index| existing_device_property_ids << index.to_i}
+    posted_device_property_ids = []
+    if params.require(:device).include?(:properties)
+      params.require(:device).fetch(:properties).each do |key, prop|
+        if !prop[:id].empty?
+          posted_device_property_ids << prop[:id].to_i
+        else
+          @device.device_properties.build(prop.permit(:name, :auto, :property_type_id, :value_type_id, :value_min, :value_max, :value))
+        end
+      end
     end
 
     # and compute which of them we need to delete
-    device_property_ids_to_delete = stored_device_property_ids - existing_device_property_ids
+    device_property_ids_to_delete = stored_device_property_ids - posted_device_property_ids
+
+    puts "STORED: #{stored_device_property_ids}"
+    puts "POSTED: #{posted_device_property_ids}"
+    puts "TO DELETE: #{device_property_ids_to_delete}"
+
+    # First delete the deleted ones
+    if device_property_ids_to_delete
+      DeviceProperty.destroy(device_property_ids_to_delete)
+    end
+
+    if posted_device_property_ids
+      device_property_post = params.require(:device).fetch(:properties)
+      puts device_property_post.inspect
+      if !@device.device_properties.empty?
+        @device.device_properties.each do |property|
+          puts property[:id].inspect
+          if stored_device_property_ids.include?(property[:id])
+            puts "NAIII"
+            device_property_post.each do |key, value|
+              if property[:id].to_i == value[:id].to_i
+                puts "FOUND"
+                puts property.inspect
+                puts value.permit(:name, :auto, :property_type_id, :value_type_id, :value_min, :value_max, :value).inspect
+                property.attributes = value.permit(:name, :auto, :property_type_id, :value_type_id, :value_min, :value_max, :value) # TODO remove the redundant permit, make it function
+                property.valid?
+                puts property.inspect
+              end
+            end
+
+          else
+            puts "OXIIIIIII"
+            # se ayti ti periptwsi den kanoyme tpt
+          end
+        end
+      end
+    end
+
+    puts @device.device_properties.inspect
 
     respond_to do |format|
       if @device.update(name: secure_params[:name], location: secure_params[:location], description: secure_params[:description])
-
-        # First delete the deleted ones
-        # TODO: find a clever way to include some filtering to not let someone delete others properties
-        if device_property_ids_to_delete
-          DeviceProperty.destroy(device_property_ids_to_delete)
-        end
-
-        # Then update the remaining existing if one
-        @device.device_properties.update(existing_device_properties.keys, existing_device_properties.values) if existing_device_properties
-
-        # Now its time to save the new properties
-        # TODO: make it better
-        if secure_params[:new_properties]
-          secure_params[:new_properties].each do |key, new_prop|
-            @device.device_properties.create(new_prop)
-          end
-        end
-
         format.html {redirect_to @device, notice: 'Device was successfully updated.'}
         format.json {render :show, status: :ok, location: @device}
       else
+        @value_types = ValueType.all
         format.html {render :edit}
         format.json {render json: @device.errors, status: :unprocessable_entity}
       end
     end
+
   end
 
   # DELETE /devices/1
@@ -160,30 +182,16 @@ class DevicesController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_device
-    @device = Device
-                  .where(user_id: current_user.id)
-                  .find(params[:uid])
+    @device = Device.where(user_id: current_user.id).find(params[:uid])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def device_params
-    params
-        .require(:device)
-        .permit(
-            :name, :location, :description,
-            new_properties:      [
-                                     :name, :auto,
-                                     :property_type_id, :value_type_id,
-                                     :value_min, :value_max,
-                                     :value,
-                                 ],
-            existing_properties: [
-                                     :name, :auto,
-                                     :property_type_id, :value_type_id,
-                                     :value_min, :value_max,
-                                     :value,
-                                 ],
-        )
+    params.require(:device).permit(:name, :location, :description, properties: [:name, :auto, :property_type_id, :value_type_id, :value_min, :value_max, :value])
   end
+  #
+  # def device_property_safe_params
+  #   params.require(:device).fetch(:properties).permit(:name, :auto, :property_type_id, :value_type_id, :value_min, :value_max, :value)
+  # end
 
 end
