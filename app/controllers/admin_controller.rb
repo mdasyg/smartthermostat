407eeed138c5775b4f7e02eed2b01a99c9810f99
Anastasios Loumpas <a.loumpas@gmail.com>
2017-05-28 13:17:34 +0300

    Make value types to post

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,4 +5,16 @@ class AdminController < ApplicationController
     @value_types = ValueType.all
   end
 
+  def updateValueTypes
+
+
+    puts 'HIII'
+
+
+    respond_to do |format|
+      format.html {redirect_to admin_url, notice: 'Value Types was successfully updated.'}
+      # format.json {render :show, status: :created, location: @device}
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # post 'test', to: 'static_pages#test'
 
   get '/admin', to: 'admin#index'
+  post '/admin/valueTypes', to: 'admin#updateValueTypes'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
