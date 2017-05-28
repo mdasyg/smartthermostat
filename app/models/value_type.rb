d16e8c681383fa7cb521557ff4f0be2c51d6bd63
Anastasios Loumpas <a.loumpas@gmail.com>
2017-05-28 12:02:12 +0300

    Fix the value type form, add buttons for new and remove

--- app/models/value_type.rb
+++ app/models/value_type.rb
@@ -2,7 +2,11 @@ class ValueType < ApplicationRecord
   PRIMITIVE_TYPES = {
       INTEGER: { ID: 1, LABEL: 'Integer' },
       FLOAT:   { ID: 2, LABEL: 'Float' },
+      STRING:  { ID: 3, LABEL: 'String' },
+      TEXT:    { ID: 4, LABEL: 'Text' },
   }
+
+  validates :name, :primitive_type_id, :unsigned, presence: true
 
   has_many :device_properties, inverse_of: :value_type
 
