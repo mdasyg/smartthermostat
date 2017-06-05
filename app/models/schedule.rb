b4bc736b433e2ba0ee46ce1752bf44c7d26d688d
Anastasios Loumpas <a.loumpas@gmail.com>
2017-06-05 21:44:40 +0300

    Add reccurency period indicator | Fix device select on schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,5 +1,14 @@
 class Schedule < ApplicationRecord
 
+  REPAT_EVERY = {
+      MINUTE: { ID: 1, LABEL: 'Minute' },
+      HOUR:   { ID: 2, LABEL: 'Hour' },
+      DAY:    { ID: 3, LABEL: 'Day' },
+      WEEK:   { ID: 4, LABEL: 'Week' },
+      MONTH:  { ID: 5, LABEL: 'Month' },
+      YEAR:   { ID: 6, LABEL: 'Year' },
+  }
+
   belongs_to :user, inverse_of: :schedules
   belongs_to :device, foreign_key: :device_uid, primary_key: :uid, inverse_of: :schedules
   has_one :event, inverse_of: :schedule
--- db/migrate/20170527080931_create_schedules.rb
+++ db/migrate/20170527080931_create_schedules.rb
@@ -4,11 +4,12 @@ class CreateSchedules < ActiveRecord::Migration[5.0]
     create_table :schedules, id: false do |t|
       t.integer :id, null: false, primary_key: true, unsigned: true, auto_increment: true
       t.integer :user_id, null: false, unsigned: true
-      t.bigint  :device_uid, null: false, unsigned: true
-      t.integer  :event_id, null: false, unsigned: true
+      t.bigint :device_uid, null: false, unsigned: true
+      t.integer :event_id, null: false, unsigned: true
       t.datetime :datetime
-      t.integer(:is_recurrent, null: false, unsigned: true, limit: 1)
-      t.integer(:recurrence_period, unsigned: true, comment: 'Measured in seconds')
+      t.integer :is_recurrent, null: false, unsigned: true, limit: 1
+      t.integer :repeat_every, null: false, unsigned: true, limit: 1
+      t.integer :recurrence_period, unsigned: true, comment: 'Measured in what the "repeat_every" says'
     end
 
     add_foreign_key :schedules, :users
