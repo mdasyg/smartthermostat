0c6e5eda9814cf82c6ce43203a56bae08919cde0
Anastasios Loumpas <a.loumpas@gmail.com>
2017-06-05 12:15:17 +0300

    Fix value types create/update remains delete

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,14 +2,61 @@ class AdminController < ApplicationController
   before_action :authenticate_user!
 
   def index
+
+    @value_types_primitive_types = []
+    ValueType::PRIMITIVE_TYPES.each do |key, p_type|
+      @value_types_primitive_types << [p_type[:LABEL], p_type[:ID]]
+    end
+
     @value_types = ValueType.all
+
   end
 
   def manage_value_types
-    respond_to do |format|
-      format.html {redirect_to admin_url, notice: 'Value Types was successfully updated.'}
-      # format.json {render :show, status: :created, location: @device}
+
+    models_has_errors = false
+
+    @value_types = []
+
+    if params.include?(:value_types)
+      params.fetch(:value_types).each do |key, valueType|
+        if valueType[:id].empty?
+          # this is a new entry
+          new_value_type = ValueType.new(safe_value_type_params(valueType))
+          if new_value_type.invalid?
+            models_has_errors = true
+          end
+          new_value_type.save
+          @value_types << new_value_type
+        else
+          # this is an old entry so update
+          old_value_type            = ValueType.find(valueType[:id])
+          old_value_type.attributes = safe_value_type_params(valueType)
+          if old_value_type.invalid?
+            models_has_errors = true
+          end
+          old_value_type.save
+          @value_types << old_value_type
+        end
+      end
     end
+
+    respond_to do |format|
+      if !models_has_errors
+        format.html {redirect_to admin_url, notice: 'Value Types was successfully updated.'}
+        # format.json {render :show, status: :created, location: @device}
+      else
+        format.html render :index
+        # format.json {render json: @device.errors, status: :unprocessable_entity}
+      end
+    end
+
+  end
+
+  private
+
+  def safe_value_type_params(unsafe_value_type)
+    unsafe_value_type.permit(:name, :primitive_type_id, :unsigned)
   end
 
 end
