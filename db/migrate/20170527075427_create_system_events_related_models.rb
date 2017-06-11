f9a9bc9a5b9e1306caba25e2bae772106d495885
Anastasios Loumpas <a.loumpas@gmail.com>
2017-06-11 11:29:18 +0300

    Fix schedule update, with actions delete inside transaction
    If validation errors occurs you have to re-delete the actions before POST

--- db/migrate/20170527075427_create_system_events_related_models.rb
+++ db/migrate/20170527075427_create_system_events_related_models.rb
@@ -11,7 +11,6 @@ class CreateSystemEventsRelatedModels < ActiveRecord::Migration[5.0]
       t.integer :id, null: false, primary_key: true, unsigned: true, auto_increment: true
       t.integer :schedule_id, null: false, unsigned: true
       t.integer :action_id, null: false, unsigned: true
-      t.text :description
     end
 
     add_index(:events, [:schedule_id, :action_id], { unique: true })
