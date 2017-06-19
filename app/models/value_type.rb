a282488f1cd94604547f890270ff5193d2caf2c0
Anastasios Loumpas <a.loumpas@gmail.com>
2017-06-19 19:16:29 +0300

    Fix value types

--- app/models/value_type.rb
+++ app/models/value_type.rb
@@ -1,15 +1,15 @@
 class ValueType < ApplicationRecord
   PRIMITIVE_TYPES = {
       INTEGER: { ID: 1, LABEL: 'Integer' },
-      FLOAT:   { ID: 2, LABEL: 'Float' },
-      STRING:  { ID: 3, LABEL: 'String' },
-      BOOL:    { ID: 4, LABEL: 'Bool' },
+      BOOL:    { ID: 2, LABEL: 'Bool' },
+      FLOAT:   { ID: 3, LABEL: 'Float' },
+      STRING:  { ID: 4, LABEL: 'String' },
       TEXT:    { ID: 5, LABEL: 'Text' },
   }
 
   validates :name, presence: true
   validates :unsigned, inclusion: { in: [true, false], message: "%{value} is not a valid state" }
-  validates :primitive_type_id, inclusion: { in: [1, 2, 3, 4], message: "'%{value}' is not a valid primitive value" }
+  validates :primitive_type_id, inclusion: { in: [1, 2, 3, 4, 5], message: "'%{value}' is not a valid primitive value" }
 
   has_many :device_attributes, inverse_of: :value_type
 
