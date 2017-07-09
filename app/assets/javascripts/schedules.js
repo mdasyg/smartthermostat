cf09431b4948e60a11616bd152c0d33dec80ad44
Anastasios Loumpas <a.loumpas@gmail.com>
2017-07-09 13:15:36 +0300

    REMOVE DATETIME form helper in order to fix a bug with format
    Clear out the schedules.js
    Correct turbolinks init from true to 'reload' (rails 5 change)

--- app/assets/javascripts/schedules.js
+++ app/assets/javascripts/schedules.js
@@ -45,49 +45,37 @@ function updateSelectedDeviceAttributes($thisElement, initActionsContainer) {
 function initializeFullCalendar() {
     $calendar = $('.calendar');
     $calendar.fullCalendar({
+        timezone: false,
         header: {
             left: 'prev,next today',
             center: 'title',
-            right: 'month,agendaWeek,agendaDay'
+            right: 'month,agendaWeek,agendaDay listMonth'
         },
         selectable: true,
         selectHelper: true,
         editable: true,
         eventLimit: true,
-
         events: '/schedules.json',
-
         select: function (start, end, jsEvent) {
-
+            $('#schedule_datetime').val(moment(start).format('YYYY-MM-DD HH:mm'));
             $('#my-modal').modal('show');
-
-            // $.getScript('/events/new', function () {
-            //     $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
-            //     date_range_picker();
-            //     $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
-            //     $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
-            // });
-
             $calendar.fullCalendar('unselect');
         },
-
     });
 }
 
+// $(document).ready( function () {
 $(document).on("turbolinks:load", function () {
-
     initializeFullCalendar();
 
     $('#schedule_datetime').datetimepicker({
-        debug: true,
-        locale: 'en',
+        // locale: 'en',
         format: "DD/MM/YYYY HH:mm:ss",
         extraFormats: ['YYYY-MM-DD HH:mm:ss'],
-        // showClear: true,
+        showClear: true,
         sideBySide: true,
-        // defaultDate: moment()
+        defaultDate: moment()
     });
-    // $('#schedule_datetime').data('DateTimePicker').format("DD/MM/YYYY HH:mm:ss")
 
     $('.schedule-device-selection').on('change', function (event) {
         updateSelectedDeviceAttributes($(this), true);
