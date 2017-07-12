5b3bde4dcc4c4c03aad4671ad081c1bc5b2b029a
Anastasios Loumpas <a.loumpas@gmail.com>
2017-07-12 18:05:50 +0300

    Add jquery ujs

--- db/schema.rb
+++ db/schema.rb
@@ -12,97 +12,98 @@
 
 ActiveRecord::Schema.define(version: 20170527080931) do
 
-  create_table "actions", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer "device_attribute_id",    null: false, unsigned: true
-    t.string  "device_attribute_value", null: false
-    t.index ["device_attribute_id"], name: "fk_rails_6c4d2296a2", using: :btree
+  create_table "actions", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "device_attribute_id", null: false, unsigned: true
+    t.string "device_attribute_value", null: false
+    t.index ["device_attribute_id"], name: "fk_rails_6c4d2296a2"
   end
 
-  create_table "device_attributes", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.bigint   "device_uid",              null: false, unsigned: true
-    t.string   "name",                    null: false
-    t.integer  "value_type_id", limit: 1, null: false, unsigned: true
-    t.string   "min_value"
-    t.string   "max_value"
-    t.string   "set_value"
-    t.string   "current_value"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.index ["device_uid"], name: "fk_rails_69a3134aba", using: :btree
-    t.index ["value_type_id"], name: "fk_rails_2a67338edb", using: :btree
+  create_table "device_attributes", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "device_uid", null: false, unsigned: true
+    t.string "name", null: false
+    t.integer "value_type_id", limit: 1, null: false, unsigned: true
+    t.string "min_value"
+    t.string "max_value"
+    t.string "set_value"
+    t.string "current_value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["device_uid"], name: "fk_rails_69a3134aba"
+    t.index ["value_type_id"], name: "fk_rails_2a67338edb"
   end
 
-  create_table "device_stats", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.bigint   "device_uid",     null: false, unsigned: true
-    t.string   "stat_name",      null: false
-    t.string   "value"
-    t.string   "label"
+  create_table "device_stats", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.bigint "device_uid", null: false, unsigned: true
+    t.string "stat_name", null: false
+    t.string "value"
+    t.string "label"
     t.datetime "last_update_at"
-    t.index ["device_uid"], name: "fk_rails_e93e183788", using: :btree
+    t.index ["device_uid"], name: "fk_rails_e93e183788"
   end
 
-  create_table "devices", primary_key: "uid", id: :bigint, default: nil, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",                       null: false, unsigned: true
-    t.string   "name",                          null: false
-    t.string   "location",                      null: false
-    t.text     "description",     limit: 65535
-    t.string   "access_token"
+  create_table "devices", primary_key: "uid", id: :bigint, unsigned: true, default: nil, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id", null: false, unsigned: true
+    t.string "name", null: false
+    t.string "location", null: false
+    t.text "description"
+    t.string "access_token"
     t.datetime "last_contact_at"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.index ["access_token"], name: "index_devices_on_access_token", unique: true, using: :btree
-    t.index ["user_id"], name: "fk_rails_410b63ef65", using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["access_token"], name: "index_devices_on_access_token", unique: true
+    t.index ["user_id"], name: "fk_rails_410b63ef65"
   end
 
-  create_table "events", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "events", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "schedule_id", null: false, unsigned: true
-    t.integer "action_id",   null: false, unsigned: true
-    t.index ["action_id"], name: "fk_rails_62e0abe502", using: :btree
-    t.index ["schedule_id", "action_id"], name: "index_events_on_schedule_id_and_action_id", unique: true, using: :btree
+    t.integer "action_id", null: false, unsigned: true
+    t.index ["action_id"], name: "fk_rails_62e0abe502"
+    t.index ["schedule_id", "action_id"], name: "index_events_on_schedule_id_and_action_id", unique: true
   end
 
-  create_table "schedules", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id",                         null: false,                                                        unsigned: true
-    t.bigint   "device_uid",                      null: false,                                                        unsigned: true
+  create_table "schedules", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id", null: false, unsigned: true
+    t.bigint "device_uid", null: false, unsigned: true
     t.datetime "datetime"
-    t.integer  "is_recurrent",      limit: 1,     null: false,                                                        unsigned: true
-    t.integer  "repeat_every",      limit: 1,                                                                         unsigned: true
-    t.integer  "recurrence_period",                            comment: "Measured in what the \"repeat_every\" says", unsigned: true
-    t.text     "description",       limit: 65535
-    t.index ["device_uid"], name: "fk_rails_df1268bd4e", using: :btree
-    t.index ["user_id"], name: "fk_rails_3c900465fa", using: :btree
+    t.string "title", null: false
+    t.integer "is_recurrent", limit: 1, null: false, unsigned: true
+    t.integer "repeat_every", limit: 1, unsigned: true
+    t.integer "recurrence_period", comment: "Measured in what the \"repeat_every\" says", unsigned: true
+    t.text "description"
+    t.index ["device_uid"], name: "fk_rails_df1268bd4e"
+    t.index ["user_id"], name: "fk_rails_3c900465fa"
   end
 
-  create_table "users", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                              null: false
-    t.string   "encrypted_password",                 null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "email", null: false
+    t.string "encrypted_password", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0, null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0, null: false
-    t.string   "unlock_token"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
     t.datetime "locked_at"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true, using: :btree
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
-  create_table "value_types", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "value_types", id: :integer, limit: 1, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "primitive_type_id", limit: 1, null: false, unsigned: true
-    t.string  "name",                        null: false
-    t.boolean "unsigned",                    null: false
+    t.string "name", null: false
+    t.boolean "unsigned", null: false
     t.integer "min_value"
     t.integer "max_value"
   end
