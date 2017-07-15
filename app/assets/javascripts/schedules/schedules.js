5c08c557980760fd900920d0f81e9c16bf5a8f2a
Anastasios Loumpas <a.loumpas@gmail.com>
2017-07-15 18:53:16 +0300

    Fix schedule event creation

--- app/assets/javascripts/schedules/schedules.js
+++ app/assets/javascripts/schedules/schedules.js
@@ -1,6 +1,5 @@
 var $scheduleFormContainer = null;
 var $scheduleForm = null;
-var $addNewEventToScheduleSelector = null;
 var $addNewEventToScheduleContainer = null;
 var $scheduleEventsContainer = null;
 var $scheduleStartDatetime = null;
@@ -9,6 +8,8 @@ var $scheduleReccurenceSettings = null;
 var $scheduleSubFormsTemplates = null;
 var $scheduleEventSubFormTemplate = null;
 var $scheduleEventActionSubFormTemplate = null;
+var $scheduleEventDeviceSelector = null;
+var $calendar = null;
 
 function replaceUrlParams(url, data = {}) {
     if (!url) {
@@ -26,7 +27,7 @@ function replaceUrlParams(url, data = {}) {
 }
 
 function initAddNewEventSelect2() {
-    $addNewEventToScheduleSelector.select2({
+    $scheduleEventDeviceSelector.select2({
         ajax: {
             dataType: 'json',
             url: $('#devices-search-url').data('url'),
@@ -37,7 +38,7 @@ function initAddNewEventSelect2() {
             },
             delay: 250
         },
-        minimumInputLength: 1,
+        minimumInputLength: 2,
         dropdownParent: $addNewEventToScheduleContainer,
         placeholder: 'Select device',
         allowClear: true,
@@ -67,7 +68,74 @@ function initScheduleDatetimePickers() {
     });
 }
 
+function initializeFullCalendar() {
+    $calendar.fullCalendar({
+        timezone: false,
+        header: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'month,agendaWeek,agendaDay listMonth'
+        },
+        selectable: true,
+        selectHelper: true,
+        editable: true,
+        eventLimit: true,
+        // events: '/schedules.json',
+        select: function (start, end, jsEvent) {
+            $('#schedule_datetime').data('DateTimePicker').date(start);
+            $scheduleModal.modal('show');
+            $calendar.fullCalendar('unselect');
+        },
+        eventClick: function (calEvent, jsEvent, view) {
+
+            // console.log(calEvent);
+            // console.log(calEvent.id);
+            // console.log(calEvent.device_uid);
+            // console.log(calEvent.name);
+            // console.log(calEvent.start.format('YYYY-MM-DD HH:mm:ss'));
+
+            $scheduleId.val(calEvent.id);
+            $scheduleForm.find('#schedule_device_uid').val(calEvent.device_uid);
+            $scheduleForm.find('#schedule_title').val(calEvent.name);
+            $scheduleForm.find('#schedule_datetime').data('DateTimePicker').date(calEvent.start);
+
+            updateSelectedDeviceAttributes($('.schedule-device-selector'), false, calEvent.actions);
+
+
+            $scheduleModal.modal('show');
+
+            return false;
+        },
+        // eventDrop: function (event, delta, revertFunc) {
+        //
+        //     console.log(event.title + " was dropped on " + event.start.format());
+        //
+        //     if (!confirm("Are you sure about this change?")) {
+        //         revertFunc();
+        //     }
+        //
+        // }
+    });
+}
+
 function addNewScheduleEvent() {
+    let deviceUid = $scheduleEventDeviceSelector.val();
+    if (!deviceUid) {
+        alert('Please select a device.');
+        return false;
+    }
+
+    let deviceAlreadyExists = false;
+    $scheduleEventsContainer.find('.schedule-event').each(function () {
+        if ($(this).data('device-uid') == deviceUid) {
+            deviceAlreadyExists = true;
+        }
+    });
+    if (deviceAlreadyExists) {
+        alert('Device already exists on schedule.');
+        return false;
+    }
+
     let lastScheduleEventDomId = parseInt($scheduleEventsContainer.find('.schedule-event').last().attr('data-index'));
     if (!lastScheduleEventDomId) {
         lastScheduleEventDomId = 0;
@@ -77,9 +145,11 @@ function addNewScheduleEvent() {
     let $newScheduleEvent = $scheduleEventSubFormTemplate.clone();
 
     $newScheduleEvent.attr('data-index', nextScheduleEventDomId);
+    $newScheduleEvent.attr('data-device-uid', $scheduleEventDeviceSelector.val());
+    $newScheduleEvent.find('.schedule-event-device-name-placeholder').text($scheduleEventDeviceSelector.find('option:selected').text());
 
     $newScheduleEvent.removeClass('odd, even');
-    if (nextScheduleEventDomId % 2 == 1) {
+    if ((nextScheduleEventDomId % 2) == 1) {
         $newScheduleEvent.addClass('odd');
     } else {
         $newScheduleEvent.addClass('even');
@@ -100,7 +170,6 @@ function addNewScheduleEvent() {
         }
     });
 
-    console.log(nextScheduleEventDomId);
     $scheduleEventsContainer.append($newScheduleEvent);
 }
 
@@ -130,8 +199,8 @@ function scheduleRecurrenceFieldsVisibilityToggle(event) {
 $(document).on('turbolinks:load', function () {
     $scheduleFormContainer = $('#schedule-form-container');
     $scheduleForm = $scheduleFormContainer.find('#schedule-form');
+    $scheduleEventDeviceSelector = $scheduleForm.find('#schedule-event-device-selector');
     $scheduleEventsContainer = $scheduleForm.find('.schedule-events-container');
-    $addNewEventToScheduleSelector = $('#add-new-event-to-schedule');
     $addNewEventToScheduleContainer = $('#add-new-event-to-schedule-container');
     $scheduleStartDatetime = $('#schedule_start_datetime');
     $scheduleEndDatetime = $('#schedule_end_datetime');
@@ -139,10 +208,11 @@ $(document).on('turbolinks:load', function () {
     $scheduleSubFormsTemplates = $('#schedule-subforms-templates');
     $scheduleEventSubFormTemplate = $scheduleSubFormsTemplates.find('.shcedule-event-template .schedule-event');
     $scheduleEventActionSubFormTemplate = $scheduleSubFormsTemplates.find('.schedule-event-action-template .action');
+    $calendar = $('.calendar');
 
     initAddNewEventSelect2();
-
     initScheduleDatetimePickers();
+    initializeFullCalendar();
 
     $scheduleReccurenceSettings.on('change', scheduleRecurrenceFieldsVisibilityToggle);
 
