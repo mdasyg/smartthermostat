53bfdc96b7785456a893dcbed564d396306ba7cf
Anastasios Loumpas <a.loumpas@gmail.com>
2017-07-16 16:06:12 +0300

    Create schedule | Load the schedule view

--- app/assets/javascripts/action.js
+++ app/assets/javascripts/action.js
@@ -1,27 +1,4 @@
-function addNewAction($containerToAppendActions, $actionTemplate, prefixForActionSubForm) {
-    // function addNewAction($thisElement, actionData) {
-    // // let $thisForm = $thisElement.closest('form');
-    // // let deviceUid = $thisForm.find('.schedule-device-selector').val();
-    // //
-    // // if (!deviceUid) {
-    // //     alert('Please select a device.');
-    // //     return false;
-    // // }
-    // //
-    // // if ($.isEmptyObject(selectedDeviceAttributes)) {
-    // //     alert('Device has no attributes, please add some first.');
-    // //     return false;
-    // // }
-    // //
-    // // let $actionsContainer = $thisForm.find('.actions-container');
-    // //
-    // // let numberOfDeviceAttributes = Object.keys(selectedDeviceAttributes).length;
-    // // let numberOfActions = $actionsContainer.children('.action').length;
-    // // if (numberOfActions >= numberOfDeviceAttributes) {
-    // //     alert('Device has only (' + numberOfDeviceAttributes + ') attributes available.');
-    // //     return false;
-    // // }
-    //
+function addNewAction($containerToAppendActions, $actionTemplate, prefixForActionSubForm, actionData) {
     let lastActionDomId = parseInt($containerToAppendActions.find('.action').last().attr('data-index'));
     if (!lastActionDomId) {
         lastActionDomId = 0;
@@ -53,35 +30,14 @@ function addNewAction($containerToAppendActions, $actionTemplate, prefixForActio
         }
     });
 
-    let $newActionSelects = $newAction.find('select');
-    $newActionSelects.each(function () {
-        propertyName = $(this).attr('name');
-        if (propertyName) {
-            propertyName = propertyName.replace('REPLACE_WITH_SCHEDULE_EVENT_FORM_PREFIX', prefixForActionSubForm);
-            propertyName = propertyName.replace('ACTION_INDEX', nextActionDomId);
-            $(this).attr('name', propertyName);
-        }
-        propertyId = $(this).attr('id');
-        if (propertyId) {
-            propertyId = propertyId.replace('REPLACE_WITH_SCHEDULE_EVENT_FORM_PREFIX', prefixForActionSubForm);
-            propertyId = propertyId.replace('ACTION_INDEX', nextActionDomId);
-            propertyId = propertyId.replace('[', '_');
-            propertyId = propertyId.replace(']', '_');
-            $(this).attr('id', propertyId);
-        }
-    });
-    //
-    // let newSelectOptions = [];
-    // $.each(selectedDeviceAttributes, function (key, value) {
-    //     newSelectOptions.push($('<option>').attr('value', key).text(value));
-    // });
-    // $newAction.find('.action-device-attribute-select-box').empty().append(newSelectOptions);
-    //
-    // if (actionData) {
-    //     $newAction.find('.action-device-attribute-select-box').val(actionData.device_attribute_id);
-    //     $newAction.find('.action-device-attribute-value').val(actionData.device_attribute_value);
-    // }
-    //
+    console.log(actionData);
+
+    if (actionData) {
+        $newAction.find('.action-device-attributee-id').val(actionData.deviceAttributeId);
+        $newAction.find('.schedule-event-action-device-attribute-name-placeholder').text(actionData.deviceAttributeName);
+        // $newAction.find('.action-device-attribute-value').val(actionData.device_attribute_value);
+    }
+
     $containerToAppendActions.append($newAction);
 
 }
--- app/models/action.rb
+++ app/models/action.rb
@@ -1,6 +1,6 @@
 class Action < ApplicationRecord
 
-  validates :device_attribute_value, presence: true
+  # validates :device_attribute_start_value, presence: true
 
   belongs_to :device_attribute
 
