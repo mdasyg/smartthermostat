39e47a30b73ced61ab430c68859d6d0bbca98a08
Anastasios Loumpas <a.loumpas@gmail.com>
2017-08-03 16:02:38 +0300

    Schedule destroy & schedule event remove seems to working

--- app/models/schedule_event.rb
+++ app/models/schedule_event.rb
@@ -4,6 +4,6 @@ class ScheduleEvent < ApplicationRecord
   belongs_to :schedule
 
   has_many :schedule_events_actions
-  has_many :actions, through: :schedule_events_actions, autosave: true
+  has_many :actions, through: :schedule_events_actions, dependent: :destroy, autosave: true
 
 end
