f7e6fba9f3fa6bcefaa6ac80a2cd5d904a767364
Anastasios Loumpas <a.loumpas@gmail.com>
2017-08-05 13:56:13 +0300

    Trying to handle overlap schedules

--- db/migrate/20170527060931_create_schedule_related_tables.rb
+++ db/migrate/20170527060931_create_schedule_related_tables.rb
@@ -10,6 +10,8 @@ class CreateScheduleRelatedTables < ActiveRecord::Migration[5.0]
       t.datetime :start_datetime, null: false
       t.datetime :end_datetime, null: false
 
+      t.integer :priority, unsigned: true, limit: 1
+
       t.integer :is_recurrent, null: false, unsigned: true, limit: 1
       t.integer :repeat_every, unsigned: true, limit: 1
       t.integer :recurrence_period, unsigned: true, comment: 'Measured in what the "repeat_every" says'
