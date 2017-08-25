ead5d5a28fe1c98016d6f60746378eeb47289f08
Anastasios Loumpas <a.loumpas@gmail.com>
2017-08-25 19:48:12 +0300

    Fix empty values when updating device through ajax

--- app/assets/javascripts/devices/devices.js
+++ app/assets/javascripts/devices/devices.js
@@ -3,12 +3,10 @@ function updateDevicePage(data) {
     let $deviceAttributeElement = null;
     data.device_attributes.forEach(function (deviceAttributeData) {
         $deviceAttributeElement = $('#device-attributes-container .device-attribute[data-id="' + deviceAttributeData.id + '"]');
-        $deviceAttributeElement.find('.device-attribute-name').text(deviceAttributeData.name);
-        $deviceAttributeElement.find('.device-attribute-min-value').text(deviceAttributeData.min_value);
-        $deviceAttributeElement.find('.device-attribute-max-value').text(deviceAttributeData.max_value);
-        if (deviceAttributeData.set_value) {
-            $deviceAttributeElement.find('.device-attribute-set-value').text(deviceAttributeData.set_value);
-        }
+        $deviceAttributeElement.find('.device-attribute-name').text((deviceAttributeData.name) ? deviceAttributeData.name : '-');
+        $deviceAttributeElement.find('.device-attribute-min-value').text((deviceAttributeData.min_value) ? deviceAttributeData.min_value : '-');
+        $deviceAttributeElement.find('.device-attribute-max-value').text((deviceAttributeData.max_value) ? deviceAttributeData.max_value : '-');
+        $deviceAttributeElement.find('.device-attribute-set-value').text((deviceAttributeData.set_value) ? deviceAttributeData.set_value : 'Empty');
         $deviceAttributeElement.find('.device-attribute-current-value').text(deviceAttributeData.current_value_text);
     });
 }
