1d391ca2c4bfc67303dbbbb5cd1727edea7fad9b
Anastasios Loumpas <a.loumpas@gmail.com>
2017-09-02 13:22:51 +0300

    Quick button updates upon changes | Small fixes

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( search.js )
 
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap/dist/css') # in order to publish all files and browser can find the source map
