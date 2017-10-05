72cb814ad6a87b5e167a3e3877085d8cba3f74ef
Anastasios Loumpas <a.loumpas@gmail.com>
2017-10-05 10:47:38 +0300

    Device status coloring | Prepare footer | Prepare home page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
 
-	# Returns the full title on a per-page basis.
+  # Returns the full title on a per-page basis.
   def full_title(page_title = '')
-    base_title = "Device Manager"
+    base_title = 'Smart Devices Control System'
     if page_title.empty?
       base_title
     else
